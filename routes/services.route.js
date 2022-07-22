a5bdaf20ac194eb987fe4ed06551e2502d63dc6d
viskhan <viskhan.rkb@gmail.com>
2022-07-22 15:29:37 +0300

    изменил assets на images

--- routes/services.route.js
+++ routes/services.route.js
@@ -7,7 +7,7 @@ const authMiddleware = require("../middleware/authMiddleware")
 const router = Router();
 
 router.get("/services", servicesController.getServices);
-router.post("/services", authMiddleware, fileMiddleware.single("assets"), servicesController.createServices);
+router.post("/services", authMiddleware, fileMiddleware.single("images"), servicesController.createServices);
 router.patch("/services/:id", authMiddleware, servicesController.updateServices);
 router.delete("/services/:id", authMiddleware, servicesController.deleteServices);
 
