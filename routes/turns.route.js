29d198dbae218885dcee5aacddcfd3a76808df70
Djabrail Magamadov <magamadov.ru095@gmail.com>
2022-07-28 16:41:44 +0300

    Исправил модель

--- routes/turns.route.js
+++ routes/turns.route.js
@@ -4,5 +4,6 @@ const { turnsController } = require("../controllers/turns.controller")
 const router = Router()
 
 router.post('/turn', turnsController.postTurns)
+router.get('/turn', turnsController.getTurn)
 
 module.exports = router
