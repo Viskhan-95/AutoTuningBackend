02cba1bb230bf6a34d83c461a257ef09c999c02e
JuniorTIM <musa.aliev06@yandex.ru>
2022-07-28 12:34:26 +0300

    отзывы под услугу

--- routes/reviews.route.js
+++ routes/reviews.route.js
@@ -4,8 +4,8 @@ const authMiddleware = require("../middleware/authMiddleware")
 
 const router = Router();
 
-router.get('/reviews', reviewController.getReview);
-router.post('/reviews', authMiddleware, reviewController.postReview);
+router.get('/reviews/:id', reviewController.getReview);
+router.post('/reviews/:id', authMiddleware, reviewController.postReview);
 router.patch('/reviews/:id', authMiddleware, reviewController.patchReview);
 router.delete('/reviews/:id', authMiddleware, reviewController.delReview);
 
