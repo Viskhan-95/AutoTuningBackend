c78c2df86687ba97f17c6b7f5b45ee7287b30bfe
viskhan <viskhan.rkb@gmail.com>
2022-07-29 10:02:25 +0300

    реализовал возможность подтверждения регистрации через email

--- models/User.model.js
+++ models/User.model.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        unique: true
+    },
     login: {
         type: String,
         unique: true
