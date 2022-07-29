f386c39ef3eca0dc57e8b1b90bf7542f41a5ac23
Djabrail Magamadov <magamadov.ru095@gmail.com>
2022-07-29 15:33:35 +0300

    Добавил контроллер для удаления услуги

--- routes/turns.route.js
+++ routes/turns.route.js
@@ -5,5 +5,6 @@ const router = Router()
 
 router.post('/turn', turnsController.postTurns)
 router.get('/turn', turnsController.getTurn)
+router.delete("/turn/:id", turnsController.delTurn)
 
 module.exports = router
